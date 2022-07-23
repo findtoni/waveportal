e3fcc4987ec98b9a19677b2636a550ca408db95a
Toni <35571969+findtoni@users.noreply.github.com>
2022-07-23 23:13:04 +0100

    fix: temp disable info

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@ import AWaveRepo from './components/AWaveRepo.vue';
     <AWaveLogo />
     <AWaveIntro />
     <AWaveLog />
-    <div class="w-5/6 md:w-3/4 mx-auto py-10">
-      <div class="flex justify-between items-center mx-auto left-0 right-0 bottom-4 absolute w-11/12 md:w-8/12">
+    <!-- <div class="w-5/6 md:w-3/4 mx-auto py-10">
+      <div class="flex justify-between items-center mx-auto left-0 right-0 bottom-4 w-11/12 md:w-8/12">
         <AWaveRepo />
         <AWaveInfo />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
