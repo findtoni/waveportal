62dcfbf230a2ed61f03a04e74f58c46b006d1695
Toni <35571969+findtoni@users.noreply.github.com>
2023-01-25 12:56:04 +0100

    feat: preloader

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useStore } from './store';
 import ALogo from './components/ALogo.vue';
+import ALoading from './components/base/ALoading.vue';
 import AWaveIntro from './components/AWaveIntro.vue';
 import AWaveLog from './components/AWaveLog.vue';
 import AFooter from './components/AFooter.vue';
-import { useStore } from './store';
+
+const isLoading = ref(true);
 const store = useStore();
 store.initialize();
+onMounted(() => isLoading.value = false);
 </script>
 
 <template>
   <div class="waveportal w-full h-full">
-    <ALogo />
-    <AWaveIntro />
-    <AWaveLog />
-    <AFooter />
+    <div v-if="isLoading" class="flex justify-center items-center h-full">
+      <ALoading is-large />
+    </div>
+    <div v-else>
+      <ALogo />
+      <AWaveIntro />
+      <AWaveLog />
+      <AFooter />
+    </div>
   </div>
 </template>
 
